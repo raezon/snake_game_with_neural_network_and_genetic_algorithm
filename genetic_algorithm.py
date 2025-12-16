f816d2f39c13b59dc528bc0d3823be468c3c6e8f
raezon <amardjebabla10@gmail.com>
2025-12-16 15:55:41 +0100

    Intialization

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -12,23 +12,11 @@ class GeneticAlgorithm:
         self.history = []
 
     def evaluate(self):
-        for s in self.population:
-            s.calculate_fitness()
+        return true
 
     def select(self):
-        self.population.sort(key=lambda s: s.fitness, reverse=True)
-        self.best_fitness = self.population[0].fitness
-        self.history.append(self.best_fitness)
-        return self.population[:self.size//4]
+       
+        return []
 
     def reproduce(self, selected):
-        new_pop = selected[:5]  # élitisme
-
-        while len(new_pop) < self.size:
-            p1, p2 = random.sample(selected, 2)
-            child_net = NeuralNetwork.crossover(p1.network, p2.network)
-            child_net.mutate()
-            new_pop.append(Snake(child_net))
-
-        self.population = new_pop
-        self.generation += 1
+        return []
